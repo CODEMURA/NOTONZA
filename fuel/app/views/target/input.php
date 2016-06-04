b79fbfbff7b47bad25b938b82671893fa7126030
Yuichi YOSHITAKA <yoshitaka@sorich.jp>
2016-06-04 16:43:10 +0900

    Refactore インデント修正

--- fuel/app/views/target/input.php
+++ fuel/app/views/target/input.php
@@ -1,36 +1,36 @@
-    <div class="wrapper">
-        <section id="form" class="contents">
-            <div class="contents__inner">
-                <h1 class="contents__title">目標金額</h1>
-                <div class="formBox">
-                    <form name="" action="balance" method="post">
-                        <p class="formBox__text">目標金額の設定</p>
-                        <div class="inputBox">
-                            <table class="formTable">
-                                <tr>
-                                    <th class="formTable__title">¥</th>
-                                    <td>
-                                        <input name="target" id="target" class="formTable__input" value="" placeholder="例：300,000">
-                                    </td>
-                                </tr>
-                            </table>
-                        </div>
-                        <p class="formBox__text">目標達成月の設定</p>
-                        <div class="inputBox">
-                            <select name="year_to">
-                                <?= $years ?>
-                            </select>
-                            年
-                            <select name="month_to">
-                                <?= $months ?>
-                            </select>
-                            月
-                        </div>
-                        <div class="buttonWrapper">
-                            <input type="submit" value="君に決めた" id="send" class="formButton">
-                        </div>
-                    </form>
-                </div>
+<div class="wrapper">
+    <section id="form" class="contents">
+        <div class="contents__inner">
+            <h1 class="contents__title">目標金額</h1>
+            <div class="formBox">
+                <form name="" action="balance" method="post">
+                    <p class="formBox__text">目標金額の設定</p>
+                    <div class="inputBox">
+                        <table class="formTable">
+                            <tr>
+                                <th class="formTable__title">¥</th>
+                                <td>
+                                    <input name="target" id="target" class="formTable__input" value="" placeholder="例：300,000">
+                                </td>
+                            </tr>
+                        </table>
+                    </div>
+                    <p class="formBox__text">目標達成月の設定</p>
+                    <div class="inputBox">
+                        <select name="year_to">
+                            <?= $years ?>
+                        </select>
+                        年
+                        <select name="month_to">
+                            <?= $months ?>
+                        </select>
+                        月
+                    </div>
+                    <div class="buttonWrapper">
+                        <input type="submit" value="君に決めた" id="send" class="formButton">
+                    </div>
+                </form>
             </div>
-        </section>
-    </div>
+        </div>
+    </section>
+</div>
--- fuel/app/views/templates/menu.php
+++ fuel/app/views/templates/menu.php
@@ -1,14 +1,14 @@
-    <div>
-        <h1>MENU</h1>
-        <ul>
-            <a href="">
-                <li>残高入力</li>
-            </a>
-            <a href="">
-                <li>残高履歴</li>
-            </a>
-            <a href="">
-                <li>サマリー</li>
-            </a>
-        </ul>
-    </div>
+<div>
+    <h1>MENU</h1>
+    <ul>
+        <a href="">
+            <li>残高入力</li>
+        </a>
+        <a href="">
+            <li>残高履歴</li>
+        </a>
+        <a href="">
+            <li>サマリー</li>
+        </a>
+    </ul>
+</div>
