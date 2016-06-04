5671ede26f848c25f7a143232bd788aa4909d0a9
Yuichi YOSHITAKA <yoshitaka@sorich.jp>
2016-06-04 17:32:26 +0900

    head内のscriptを別ファイルに分離

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -6,32 +6,7 @@
     <meta name="description" content="休日もPC">
     <link rel="stylesheet" href="assets/css/notonza.css" media="all">
     <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js"></script>
-    <script>
-        $(function () {
-
-            var date = new Date();
-            var y = date.getFullYear();
-            var m = date.getMonth() + 1;
-
-            var hideMonthBefore = function () {
-                if($('[name=year_to]').val() == y){
-
-                    for(i = 0; i < m; i++){
-                        $('[name=month_to] option').eq(i).hide();
-                    }
-                }else{
-                    for(i = 0; i < m; i++){
-                        $('[name=month_to] option').eq(i).show();
-                    }
-                }
-            };
-
-            hideMonthBefore();
-            $('[name=year_to]').change(function(){
-                hideMonthBefore();
-            });
-        });
-    </script>
+    <?= Asset::js("notonza.js"); ?>
 </head>
 <body>
 <?php echo $menu; ?>
