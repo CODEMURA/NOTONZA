1df71dc5b7d4bb38dbcedad6474c2627257ec4dc
Kangin Kim <iwakura@sorich.jp>
2016-06-04 17:09:38 +0900

    「目標月設定」現在月より以前の月を表示させない

--- fuel/app/views/template.php
+++ fuel/app/views/template.php
@@ -9,15 +9,27 @@
     <script type="text/javascript" src="js/codemura.js"></script>
     <script>
         $(function () {
-            // カンマを挿入
-            $('input').keyup(function () {
-                var num = $(this).val();
-                console.log(num);
 
-                if (num.length > 3 && num.length % 3 == 1) {
-                    console.log("４けた以上");
+            var date = new Date();
+            var y = date.getFullYear();
+            var m = date.getMonth() + 1;
 
+            var hideMonthBefore = function () {
+                if($('[name=year_to]').val() == y){
+
+                    for(i = 0; i < m; i++){
+                        $('[name=month_to] option').eq(i).hide();
+                    }
+                }else{
+                    for(i = 0; i < m; i++){
+                        $('[name=month_to] option').eq(i).show();
+                    }
                 }
+            };
+
+            hideMonthBefore();
+            $('[name=year_to]').change(function(){
+                hideMonthBefore();
             });
         });
     </script>
