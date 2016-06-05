deff4b2c27fd8d2d7220a735310f7ba74658db3e
Kangin Kim <iwakura@sorich.jp>
2016-06-05 13:50:28 +0900

    modify 現在月以前のオプションタグを非表示

--- public/assets/js/notonza.js
+++ public/assets/js/notonza.js
@@ -1,24 +1,32 @@
 $(function () {
 
+    initMonthOptionTags();
+    delimiteDigit();
+
+});
+
+function initMonthOptionTags(){
+
     var date = new Date();
     var y = date.getFullYear();
     var m = date.getMonth() + 1;
 
-    var hideMonthBefore = function () {
-        if($('[name=year_to]').val() == y){
+    var isFutureYear = function(){
+        return Number($('[name=year_to]').val()) !== y;
+    };
 
-            for(i = 0; i < m; i++){
-                $('[name=month_to] option').eq(i).hide();
-            }
-        }else{
-            for(i = 0; i < m; i++){
-                $('[name=month_to] option').eq(i).show();
-            }
+    var hidePassedMonths = function () {
+        for(i = 0; i < m; i++){
+            $('[name=month_to] option').eq(i).toggle(isFutureYear());
         }
     };
 
-    hideMonthBefore();
+    hidePassedMonths();
     $('[name=year_to]').change(function(){
-        hideMonthBefore();
+        hidePassedMonths();
     });
-});
+}
+
+function delimiteDigit(){
+
+}
